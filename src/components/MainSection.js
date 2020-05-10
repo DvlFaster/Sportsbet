be40dcab4796ac224fdad8e47f1353de47befaf8
Erol <dvl_faster@abv.bg>
2020-05-11 00:48:37 +0300

    Font size icons, updates

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -6,45 +6,32 @@ import PromoBanner from "./promo-banner/PromoBanner";
 const MainSection = props => {
 
     return (
-        <div className="d-flex product-spec">
-            <div className="product-spec-icon-container d-flex align-items-lg-start justify-content-lg-start">
-                <i className="fas fa-futbol"></i>
-                <div className="d-flex flex-column justify-content-center">
-                    <div className="product-spec-label">Main Club</div>
-                    <div className="product-spec-value">Partner</div>
-                </div>
-                <div className="d-inline main-section">
-                </div>
-
-                <div className="d-flex product-spec">
-                    <div className="product-spec-icon-container d-flex align-items-lg-start justify-content-lg-start">
-                        <i className="fas fa-futbol"></i>
-                        <div className="d-flex flex-column justify-content-start">
-                            <div className="product-spec-label">Official Shirt</div>
-                            <div className="product-spec-value">Sponsor</div>
-                        </div>
-                        <div className="d-flex main-container">
-                        </div>
+        <div className="row product-spec justify-content-lg-between">
+            <div className="col d-flex">
+                <div className="product-spec-icon-container d-inline-flex mr-2 align-items-center border-right pr-5">
+                    <i className="fas fa-futbol mr-2"></i>
+                    <div className="d-flex flex-column justify-content-center">
+                        <div className="product-spec-label">Main Club</div>
+                        <div className="product-spec-value">Partner</div>
                     </div>
                 </div>
-                {/* <div className="d-flex main-section">
-            <VideoContainer video={"https://www.youtube.com/embed/zpOULjyy-n8?rel=0"}/>
-            <EventBoard/>
-        </div>*/}
+                <div className="product-spec-icon-container d-inline-flex align-items-center border-right pr-5">
+                    <i className="fas fa-futbol mr-2"></i>
+                    <div className="d-flex flex-column justify-content-start">
+                        <div className="product-spec-label">Official Shirt</div>
+                        <div className="product-spec-value">Sponsor</div>
+                    </div>
+                </div>
             </div>
-            <div className="d-flex tags-container">
-
+            <div className="col">
                 <div className="product-spec-icon-container d-flex align-items-lg-end justify-content-lg-end">
-                    <button type="button" className="btn btn-title card-button">Sign in</button>
+                    <button type="button" className="btn btn-title card-button mr-2">Sign in</button>
                     <button type="button" className="btn btn-primary card-button">Register</button>
                     <div className="d-flex d-lg-inline-flex">
                     </div>
                 </div>
             </div>
-
         </div>
-
-
     )
 }
 export default MainSection
--- src/components/promo-banner/PromoContainer.js
+++ src/components/promo-banner/PromoContainer.js
@@ -4,34 +4,71 @@ import PromoBanner from './PromoBanner';
 const PromoContainer = props => {
 
     return (
-        <div className="promo-container d-flex">
-            <PromoBanner
-                title={"Sports"}
-                description={"Price Boost every day on all sports"}
-                image={"https://sportsbet-io.imgix.net/sportsbet-io/portals/home-app/assets/card-sports3.png"}>
-                <button type="button" className="btn promo-button">Go to Sports</button>
-            </PromoBanner>
-            <PromoBanner
-                title={"Casino"}
-                description={"Offering the best slots and live dealer games"}
-                image={"https://sportsbet-io.imgix.net/sportsbet-io/portals/home-app/assets/card-casino4.png"}
-            >
-                <button type="button" className="btn promo-button">Go to Casino</button>
-            </PromoBanner>
+        <>
+            <div className="promo-container d-flex">
+                <PromoBanner
+                    title={"Sports"}
+                    description={"Price Boost every day on all sports"}
+                    image={"https://sportsbet-io.imgix.net/sportsbet-io/portals/home-app/assets/card-sports3.png"}>
+                    <button type="button" className="btn promo-button">Go to Sports</button>
+                </PromoBanner>
+                <PromoBanner
+                    title={"Casino"}
+                    description={"Offering the best slots and live dealer games"}
+                    image={"https://sportsbet-io.imgix.net/sportsbet-io/portals/home-app/assets/card-casino4.png"}
+                >
+                    <button type="button" className="btn promo-button">Go to Casino</button>
+                </PromoBanner>
 
-            <div className="row">
-                <div className="col">
-                    <div className="description-container">
-                        <h3 className="description-label">Sport Categories</h3>
-                        <p className="description-content"></p>
 
+
+            </div>
+            <div className="promo-container">
+                <div className="row">
+                    <div className="col">
+                        <div className="description-container">
+                            <h3 className="description-label">Sport Categories</h3>
+                            <p className="description-content"></p>
+                        </div>
                     </div>
                 </div>
+                <div className="row">
+                    <div className="col">
+                        <div className="d-inline-flex flex-column">
+                            <i class="fas fa-futbol text-center bg-dark text-danger p-3 sport-icon"></i>
+                            <span className="description-content text-center text-muted">Soccer</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <div className="d-inline-flex flex-column">
+                            <i className="fas fa-futbol text-center bg-dark text-danger p-3 sport-icon"></i>
+                            <span className="description-content text-center text-muted">eSoccer</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col">
+                        <div className="d-inline-flex flex-column">
+                            <i className="fas fa-basketball-ball text-center bg-dark text-danger p-3 nba-icon"></i>
+                            <span className="description-content text-center text-muted">eNBA</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col">
+                        <div className="d-inline-flex flex-column">
+                            <i className="fas fa-table-tennis text-center bg-dark text-danger p-3 nba-icon"></i>
+                            <span className="description-content text-center text-muted">Table Tennis</span>
+                        </div>
+                    </div>
+                </div>
+
             </div>
-            {/*<h3 size="delta" color="bulma.100" className="heading__Heading-tjo9yi-0 ivFKnh">Sports categories</h3>*/}
-
-        </div>
-
+        </>
     )
 }
 export default PromoContainer
